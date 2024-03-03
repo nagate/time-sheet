db0a1ccff0050bda672e155ca1c08121ba19722e
akinori.akamine <>
2024-03-04 00:58:17 +0900

    indexeddbの値を画面に表示

--- src/app/time-sheets/page.tsx
+++ src/app/time-sheets/page.tsx
@@ -1,31 +1,67 @@
 "use client";
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridValueFormatterParams,
+} from "@mui/x-data-grid";
 import { useEffect, useRef, useState } from "react";
 import datetimeUtil from "@/utils/datetime";
 import {
-  bulkInsertTimeSheet,
   getTimeSheetByYearMonth,
   insertMonthTimeSheet,
 } from "@/services/timeSheetService";
 import { TimeSheet } from "@/types/timeSheetType";
 
 const columns: GridColDef[] = [
-  { field: "date", headerName: "日付", width: 60, sortable: false },
+  {
+    field: "date",
+    headerName: "日付",
+    width: 80,
+    sortable: false,
+    valueFormatter: (params: GridValueFormatterParams<Date>) => {
+      if (params.value == null) {
+        return "";
+      }
+      return datetimeUtil.getFormattedDatetime({
+        date: params.value,
+        format: "dd（aaa）",
+        zeroFilled: true,
+      });
+    },
+  },
   {
     field: "startTime",
     headerName: "出勤",
-    type: "dateTime",
     width: 60,
     sortable: false,
+    valueFormatter: (params: GridValueFormatterParams<Date>) => {
+      if (params.value == null) {
+        return "";
+      }
+      return datetimeUtil.getFormattedDatetime({
+        date: params.value,
+        format: "HH:mm",
+        zeroFilled: true,
+      });
+    },
   },
   {
     field: "endTime",
     headerName: "退勤",
-    type: "date",
     width: 60,
     sortable: false,
+    valueFormatter: (params: GridValueFormatterParams<Date>) => {
+      if (params.value == null) {
+        return "";
+      }
+      return datetimeUtil.getFormattedDatetime({
+        date: params.value,
+        format: "HH:mm",
+        zeroFilled: true,
+      });
+    },
   },
   {
     field: "breakTime",
@@ -34,19 +70,6 @@ const columns: GridColDef[] = [
     width: 40,
     sortable: false,
   },
-];
-
-const rows = [
-  { id: 1, date: "1(月)", start: "09:00", end: "18:00", break: 60 },
-  { id: 2, date: "88(月)", start: "24:33", end: "88:88", break: 888 },
-  // { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  // { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  // { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  // { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  // { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  // { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  // { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  // { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
 export default function TimeSheetsPage() {
@@ -94,23 +117,20 @@ export default function TimeSheetsPage() {
     getData();
   }, []);
   return (
-    <Box sx={{ height: 400, width: "100%" }}>
+    <Box sx={{ height: 500, width: "100%" }}>
+      <Box>{`${thisYear.current}/${thisMonth.current}`}</Box>
       <DataGrid
         rows={timeSheets}
         columns={columns}
-        // initialState={{
-        //   pagination: {
-        //     paginationModel: {
-        //       pageSize: 15,
-        //     },
-        //   },
-        // }}
-        // pageSizeOptions={[15]}
-        // checkboxSelection
-        // disableRowSelectionOnClick
+        loading={timeSheets.length === 0}
+        columnHeaderHeight={32}
+        rowHeight={40}
+        // autoHeight
         disableColumnFilter
         disableColumnMenu
-        hideFooterPagination
+        // hideFooterPagination
+        hideFooter
+        // autoPageSize
       />
     </Box>
   );
